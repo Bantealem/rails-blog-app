d45c9738c5eafc8984e15fbeef2d9500a77d9b8e
Bantealem <bantem2021@gmail.com>
2022-12-14 17:04:53 +0300

    Add request rspec for use and post controllers

---
+++ spec/requests/users_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.describe 'users', type: :request do
+  describe 'GET/index' do
+    before :each do
+      get '/users'
+    end
+    it 'is a success' do
+      expect(response).to have_http_status(:success)
+    end
+    it 'should render index' do
+      expect(response).to render_template(:index)
+    end
+    it 'Should return the correct placeholder' do
+      expect(response.body).to include('Users')
+    end
+  end
+
+    describe 'GET/show' do
+    before :each do
+      get '/users'
+    end
+    it 'is a success' do
+      expect(response).to have_http_status(:success)
+    end
+    it 'Should return the correct placeholder' do
+      expect(response.body).to include('Users')
+    end
+  end
+end
