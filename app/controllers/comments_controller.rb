094833e68053a2b4001569bfd7a692f5bbb2b89c
Bantealem <bantem2021@gmail.com>
2022-12-17 20:43:36 +0300

    Add forms

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,58 +1,18 @@
 class CommentsController < ApplicationController
-  before_action :set_comment, only: %i[show edit update destroy]
-
-  def index
-    @comments = Comment.all
-  end
-
-  def show; end
-
   def new
     @comment = Comment.new
+    @post = Post.find(params[:post_id])
   end
-
-  def edit; end
 
   def create
-    @comment = Comment.new(comment_params)
+    post = Post.find(params[:post_id])
+    comment = Comment.create(author: current_user, post: comment_params)
+    return unless comment.save
 
-    respond_to do |format|
-      if @comment.save
-        format.html { redirect_to comment_url(@comment), notice: 'Comment was successfully created.' }
-        format.json { render :show, status: :created, location: @comment }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @comment.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  def update
-    respond_to do |format|
-      if @comment.update(comment_params)
-        format.html { redirect_to comment_url(@comment), notice: 'Comment was successfully updated.' }
-        format.json { render :show, status: :ok, location: @comment }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @comment.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  def destroy
-    @comment.destroy
-
-    respond_to do |format|
-      format.html { redirect_to comments_url, notice: 'Comment was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to user_post_path(current_user.id, post.id), notice: 'Comment saved'
   end
 
   private
-
-  def set_comment
-    @comment = Comment.find(params[:id])
-  end
 
   def comment_params
     params.require(:comment).permit(:text)
